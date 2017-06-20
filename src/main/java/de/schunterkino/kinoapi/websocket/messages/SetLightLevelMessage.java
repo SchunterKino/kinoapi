44b3e44f40de5cd9b1f8029d8bb0fa1662a2ed2c
Jannik Hartung <januk@freenet.de>
2017-06-21 00:42:26 +0200

    Change "set_light_level" command format to the one used in the frontend
    The field is called "level" instead of "light_level" in the frontend.

--- src/main/java/de/schunterkino/kinoapi/websocket/messages/SetLightLevelMessage.java
+++ src/main/java/de/schunterkino/kinoapi/websocket/messages/SetLightLevelMessage.java
@@ -1,15 +1,15 @@
 package de.schunterkino.kinoapi.websocket.messages;
 
 public class SetLightLevelMessage extends BaseMessage {
-	private int light_level;
+	private int level;
 
-	public SetLightLevelMessage(int light_level) {
+	public SetLightLevelMessage(int level) {
 		super("lights", "set_light_level");
-		this.light_level = light_level;
+		this.level = level;
 	}
 
 	public int getLightLevel() {
-		return light_level;
+		return level;
 	}
 
 }
